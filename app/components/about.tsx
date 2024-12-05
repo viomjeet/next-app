fc30c8ecf772873cf798cb22b4f933c45b4c4f0b
vikas kumar <vikas.kumar@inn-flow.com>
2024-12-05 12:45:13 +0530

    2

---
+++ app/components/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+    <h1 className="text-6xl">About</h1>
+    <hr/>
+    <p>We recommend starting a new Next.js app using create-next-app, which sets up everything automatically for you. To create a project, run:</p>
+    </div>
+  )
+}
+
+export default about
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>page</h1>
+      <h1 className="text-6xl bg-red-400">page</h1>
       <p>We recommend starting a new Next.js app using create-next-app, which sets up everything automatically for you. To create a project, run:</p>
-      </div>
+    </div>
   )
 }
 
